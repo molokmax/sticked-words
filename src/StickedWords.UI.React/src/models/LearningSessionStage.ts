f9816c23b795762e5114e562cbcdbe27574bf742
Maxim Molokanov <maks.molokanov@gmail.com>
2026-03-28 00:00:12 +0300

    F/#63/exercise intro (#94)
    * Add get-ready screen
    
    * Version up

--- src/StickedWords.UI.React/src/models/LearningSessionStage.ts
+++ src/StickedWords.UI.React/src/models/LearningSessionStage.ts
@@ -2,6 +2,7 @@ export class LearningSessionStage {
   id!: number;
   ordNumber!: number;
   completedFlashCardCount!: number;
+  isActive!: boolean;
 
   static fromJson(json: any): LearningSessionStage {
     const result = new LearningSessionStage();
@@ -9,6 +10,7 @@ export class LearningSessionStage {
     result.id = json.id;
     result.ordNumber = json.ordNumber ?? 0;
     result.completedFlashCardCount = json.completedFlashCardCount ?? 0;
+    result.isActive = json.isActive ?? false;
 
     return result;
   }
